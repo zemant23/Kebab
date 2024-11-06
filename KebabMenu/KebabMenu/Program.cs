176950e7830f1c4b76aed7682148983b8fe0d746
Tadeáš Zeman <zemant23@spstrutnov.cz>
2024-11-06 14:19:20 +0100

    zaklad

---
+++ KebabMenu/KebabMenu/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace KebabMenu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dobrý den pro sklad surovin stiskněte S pro Menu kebabu M");
+
+        }
+    }
+}
